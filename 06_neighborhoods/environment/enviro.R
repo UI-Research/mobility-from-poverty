17fe672757a8ef75d6ff4f23a3a9e95db715fde1
peacegwam <pgwam@urban.org>
2020-08-28 16:20:22 -0400

    Updated code
    Revised code based on Aaron's review.

--- 06_neighborhoods/environment/enviro.R
+++ 06_neighborhoods/environment/enviro.R
@@ -1,7 +1,7 @@
 # enviro. R
 # pulls enviromental idicators from HUD AFFH data and population weighs observations 
 # created by Peace Gwam
-# updated on 08-17-2020
+# updated on 2020-08-27
 
 
 #install.packages("devtools")
@@ -11,11 +11,30 @@ library(tidycensus)
 library(purrr)
 library(urbnmapr)
 
-census_api_key("021b4f58d83bd96aa25f2f4fd4e5570ef37ca284", install = TRUE, overwrite = TRUE)
+#to use tidycensus, use census_api_key function to set api key. download api key from https://api.census.gov/data/key_signup.html
 
 ####STEP ONE: PULL AND MERGE DATA####
 
+# pull 2010-2014 5 yr acs data - total population per tract 
+state_fips <- unique(urbnmapr::states$state_fips)
+acs <- function(state_fips) {
+  get_acs(geography = "tract",
+          variables  = "B01003_001", 
+          year = 2014,
+          key = key,
+          state = state_fips,
+          geometry = FALSE)
+}
+acs <- map_df(state_fips, acs)
+
+# remove variable column: 
+acs <- acs %>%
+  select (-variable)
+
+
+
 # pull hud affh data on air quality
+# AFFH data is no longer available via hud.gov. use urban data catalog for access. 
 affh <- read_csv("raw/AFFH_tract_AFFHT0006_July2020.csv", 
                            col_types = cols(
                              .default = col_double(),
@@ -29,6 +48,9 @@ affh <- read_csv("raw/AFFH_tract_AFFHT0006_July2020.csv",
 enviro_stats <- affh %>%
   select(GEOID = geoid, state, state_name, county, county_name, tract, haz_idx) 
 
+# remove puerto rico from affh data because not part of analysis
+affh <- affh %>%
+  filter (state_name != "Puerto Rico")
 
 # add leading zeros to GEOID to match with the acs
 enviro_stats <- enviro_stats %>%
@@ -38,60 +60,27 @@ enviro_stats <- enviro_stats %>%
                           side = "left",
                           pad = "0"))
 
-# pull 2010-2014 5 yr acs data - total population per tract
-state_fips <- unique(urbnmapr::states$state_fips)
-my_states <- function(state_fips) {
-  get_acs(geography = "tract",
-            variables  = "B01003_001", 
-            year = 2014,
-            key = key,
-            state = state_fips,
-            geometry = FALSE)
-}
-my_states <- map_df(state_fips, my_states)
-
-# remove variable column to easily merge with Puerto Rico data below:
-my_states$variable <- NULL
-head(my_states)
-
-# add acs data for Puerto Rico - affh data not available for other territories
-puerto_rico <- get_acs(geography = "tract",
-                    variables = "B01003_001",
-                    survey = "acs5",
-                    state = "PR",
-                    cache_table = TRUE,
-                    output = "wide",
-                    year = 2014)
-
-# rename variables to match acs pull for the states + DC
-names (puerto_rico) [3] <- "estimate"
-names (puerto_rico) [4] <- "moe"
-
-
-# append acs data from all states, DC, and PR
-acs <- rbind (my_states, puerto_rico)
-
 # join acs data with affh data
-full_data <- left_join(acs, enviro_stats, by = "GEOID")
+full_data <- left_join(enviro_stats, acs, by = "GEOID")
+
 
 ####STEP TWO: VALIDATE AND CLEAN MERGED DATA ####
 
-# identify census tracts not represented in affh data - 4 in US + PR
+# identify census tracts not represented in affh data - 4 in US 
 anti_join(acs, enviro_stats, by= c("GEOID"))
 
 # drop census tracts not in affh data 
 tracts_in_affh <- filter(full_data, !GEOID %in% c("02270000100", "46113940500", "46113940800", "46113940900"))
 
 
-# there are 580 census tracts with N/A values for hazard indicies, with est. populations between 0 and 500
 # drop the census tracts with no est. population:
-tracts_with_pop<- filter(tracts_in_affh, estimate > 0)
+tracts_with_pop <- filter(tracts_in_affh, estimate > 0)
 
 # there are 26 remaining tracts that have population and no hazard information. These tracts have between 1 and 7778 people
 # tracts with no hazard indices and a population greater than zero: 
 tracts_with_pop %>%
   filter(is.na(haz_idx)) %>%
-  View
+  
 
 # the number of tracts without a hazard index and population > 0 are about 0.03% of all observations
 # proceed by dropping the census tracts without haz_idx:
@@ -121,9 +110,8 @@ county_enviro_stats <- county_enviro_stats %>%
   add_column(year = 2014, .before = "state") %>%
   mutate(state = str_pad(string = state, width = 2, side ="left", pad = "0")) %>%
   mutate(county = str_pad(string = county, width = 3, side = "left", pad = "0")) %>%
-  mutate(GEOID = str_c(state, county, collapse = NULL))
+  mutate(GEOID = str_c(state, county, collapse = NULL)) %>% 
+  select(-GEOID)
 
-
-
-
-write_csv(county_enviro_stats, "output/county_level_enivro.csv", na = "NA", append = FALSE, col_names = TRUE)
+##output data
+write_csv(county_enviro_stats, "county_level_enviro.csv")
