a72cdfa0600a444d6ab07d1f7c9742a5895b59d1
vpancini <pancinivincent@gmail.com>
2022-12-22 15:39:20 -0500

    clean file format

--- geographic-crosswalks/create_place_file.R
+++ geographic-crosswalks/create_place_file.R
@@ -7,7 +7,7 @@ library(tidycensus)
 
 # Replace YOUR-KEY-HERE below with your unique Census API key. Request a key at
 # https://api.census.gov/data/key_signup.html if you don't have one.
-# census_api_key("YOUR-KEY-HERE")
+census_api_key("866122dc573a0f65f0ff4c23130956014a5b480c")
 
 #' Get population estimates from the US Census Bureau Population Estimation Program
 #'
@@ -41,31 +41,41 @@ pop <- bind_rows(pop, census2020)
 pop <- pop %>%
   select(-variable) %>%
   rename(population = value,
-         geographicarea = NAME,
-         stateplacefp = GEOID)
+         place_state_name = NAME,
+         state_place = GEOID)
 
 
 # load in original population-based city file to get our original 486 cities
-og_cityfile <- read_csv(here::here("geographic-crosswalks", "data", "city_state_2020_population.csv")) %>%
-  rename(geographicarea = GeographicArea,
-         statefips = fips) %>%
+my_places <- read_csv(here::here("geographic-crosswalks", "data", "city_state_2020_population.csv")) %>%
   # DC is missing statefips and state_abbr
   mutate(state_abbr = case_when(cityname == "Washington city" & statename == "District of Columbia" ~ "DC",
                                 TRUE ~ state_abbr),
-         statefips = case_when(cityname == "Washington city" & statename == "District of Columbia" ~ 11,
-                               TRUE ~ statefips)) %>%
-  select(-population2020)
+         fips = case_when(cityname == "Washington city" & statename == "District of Columbia" ~ 11,
+                               TRUE ~ fips)) %>%
+  select(fips, statename, cityname, GeographicArea) %>%
+  rename(state = fips,
+         state_name = statename,
+         place_name = cityname,
+         place_state_name = GeographicArea)
 
 # join data
-joined_data <- left_join(x = og_cityfile,
+joined_data <- left_join(x = my_places,
                          y = pop,
-                         by = "geographicarea") %>%
-  arrange(year, statefips, stateplacefp) %>%
-  select(year, geographicarea, cityname, city, statename, state_abbr, population, statefips, stateplacefp)
+                         by = "place_state_name") 
+
+# prepare file for export
+joined_data <- joined_data %>%
+  mutate(state = str_sub(state_place, start = 1L, end = 2L),
+         place = str_sub(state_place, start = 3L, end = 7L)) %>%
+  select(year, state, place, state_name, place_name, population) %>%
+  arrange(year, state, place)
 
 # We have 486 cities, so for 5 years we should have 2430 observations.
 # However, South Fulton city, Georgia was incorporated in 2017, so we only
 # have population estimates for this city from 2018-2020. Therefore, there
 # are 2428 total observations
 
-write_csv(joined_data, here::here("geographic-crosswalks/data/place-populations.csv"))
+write_delim(x = joined_data,
+          file = here::here("geographic-crosswalks/data/place-populations.csv"),
+          delim = ","
+          )
