53a1e64f959030e249d2a1664393a70e331427a5
tinatinc <41295341+tinatinc@users.noreply.github.com>
2023-02-26 22:08:02 -0500

    Updated Economic Connectedness codes (county/city) according to Aaron's feedback
    Updated both code files for consistency & re-ran to update the files as well. Renamed some folders for clarity

--- 06_neighborhoods/social-capital/economic_connectedness_county_code_2022.R
+++ 06_neighborhoods/social-capital/economic_connectedness_county_code_2022.R
@@ -1,7 +1,7 @@
 ###############################################################################
 
 # Description: Code to create county-level Economic Connectedness (one of two Social Capital Gates Mobility Metrics)  
-# Data:  [gitfolder]/06_neighborhoods/social-capital/data/social_capital_county.csv (won't be on Github)
+# Data:  [gitfolder]/06_neighborhoods/social-capital/data/    .csv (won't be on Github)
 # Data downloaded from Opportunity Insights Social Capital Atlas
 # Author: Tina Chelidze											   
 # Date: September 10, 2022
@@ -14,8 +14,7 @@
 
 ###############################################################################
   
-    # Set working directory to [gitfolder]. Update path as necessary to your local metrics repo
-    setwd("C:/Users/tchelidze/Documents/GitHub/mobility-from-poverty")
+    # Set working directory to [gitfolder]: Open mobility-from-poverty.Rproj to make sure all file paths will work
 
     # Libraries you'll need
     library(sf)
@@ -27,22 +26,33 @@
 # (1) download data from socialcapital.org
   
 #     access via https://data.humdata.org/dataset/social-capital-atlas	
-#     Social Capital Atlas - US Counties.csv is saved in [gitfolder]/06_neighborhoods/social-capital/data
+
+      # Specify URL where source data file is online
+      url <- "https://data.humdata.org/dataset/85ee8e10-0c66-4635-b997-79b6fad44c71/resource/ec896b64-c922-4737-b759-e4bd7f73b8cc/download/social_capital_county.csv"
+      
+      # Specify destination where file should be saved (the .gitignore folder for your local branch)
+      destfile <- "06_neighborhoods/social-capital/temp/social_capital_county.csv"
+      
+      # Import the data file & save locally
+      download.file(url, destfile)
   
 
 # (2) open and clean the file (separate county and state codes, fill in missing zeroes)
   
      # open data
-      ec_raw <- read.csv("06_neighborhoods/social-capital/data/social_capital_county.csv")
+      ec_raw <- read.csv("06_neighborhoods/social-capital/temp/social_capital_county.csv")
 
      # add leading zeroes where they are missing (2-digit state FIP + 3-digit county FIP  = 5 digit code)
-      ec_raw$county <- sprintf("%05d", as.numeric(ec_raw$county))
+      ec_raw <- ec_raw %>%
+        mutate(county = sprintf("%0.5d", as.numeric(county)))
   
      # keep only relevant data
-      ec_raw <- ec_raw %>% select(county, county_name, ec_county, ec_se_county)
+      ec_raw <- ec_raw %>% 
+        select(county, county_name, ec_county, ec_se_county)
       
      # remove observations with missing data for our EC variable
-      ec_raw %>% drop_na(ec_county)
+      ec_raw <- ec_raw %>% 
+        drop_na(ec_county)
 
      # rename the FIPS variable to avoid confusion
       ec_raw <- ec_raw %>% 
@@ -51,16 +61,18 @@
      # create a new column for the state FIPS
       ec_raw$state <- as.numeric(substr(ec_raw$totalFIPS, 1, 2))
      # add in the lost leading zeroes
-      ec_raw$state <- sprintf("%02d", as.numeric(ec_raw$state))
+      ec_raw <- ec_raw %>%
+        mutate(state = sprintf("%0.2d", as.numeric(state)))
      # create a new column for the county FIPS
       ec_raw$county <- as.numeric(substr(ec_raw$totalFIPS, 3, 5))
      # add in the lost leading zeroes
-      ec_raw$county <- sprintf("%03d", as.numeric(ec_raw$county))
+      ec_raw <- ec_raw %>%
+        mutate(county = sprintf("%0.3d", as.numeric(county)))
      # create a column for the year of this data
       ec_raw$year <- "2022"
       
-     # save as temporary data file
-      write.csv(ec_raw,"06_neighborhoods/social-capital/data/social-capital-county-clean.csv", row.names = FALSE)
+     # save as temporary data file in the same .gitignore folder
+      write_csv(ec_raw,"06_neighborhoods/social-capital/temp/social-capital-county-clean.csv")
       
 
 # (3)  use crosswalk to check any missing counties	
@@ -69,15 +81,21 @@
       county_pop <- read.csv("geographic-crosswalks/data/county-populations.csv")
       
       # add in the lost leading zeroes for the county FIP
-      county_pop$county <- sprintf("%03d", as.numeric(county_pop$county))
+      county_pop <- county_pop %>%
+        mutate(county = sprintf("%0.3d", as.numeric(county)))
       # add in the lost leading zeroes for the state FIP
-      county_pop$state <- sprintf("%02d", as.numeric(county_pop$state))
+      county_pop <- county_pop %>%
+        mutate(state = sprintf("%0.2d", as.numeric(state)))
 
       # keep the most recent year of population data (not 2022, but 2020)
       county_pop <- filter(county_pop, year > 2019)
       
       # merge the county file into the ec data file (left join, since county file has more observations)
-      merged_ec <- merge(county_pop, ec_raw, by=c("state", "county"), all.x = TRUE)
+      merged_ec <- left_join(county_pop, ec_raw, by=c("state", "county"))
+      # sort by state county
+      merged_ec <- merged_ec[
+        with(merged_ec, order(state, county)),
+      ]
       
       # check how many missing values (counties without EC data)
       sum(is.na(merged_ec$ec_county))
@@ -94,14 +112,17 @@
       # keep only relevant data (dropping population data, keeping EC data year only)
       merged_ec <- merged_ec %>% select(year.y, state, county, state_name, county_name.x, ec_county, ec_se_county, data_quality)
       
+      
       # rename the needed variable to avoid confusion
       merged_ec <- merged_ec %>% 
         rename("county_name" = "county_name.x")
       merged_ec <- merged_ec %>% 
         rename("year" = "year.y")
+      merged_ec <- merged_ec %>% 
+        rename("_quality" = "data_quality")
       
       # explort as .csv
-      write_csv(merged_ec, "06_neighborhoods/social-capital/final_data/economic_connectedness_county_2022.csv")
+      write_csv(merged_ec, "06_neighborhoods/social-capital/data/economic_connectedness_county_2022.csv")
       
       
       
