665695dfdba4bd97b9066261f63a4aef493b73b4
tinatinc <41295341+tinatinc@users.noreply.github.com>
2023-03-07 13:06:54 -0500

    Microdata, Income, Employment, College, Preschool, and Rent code + data
    also added relevant crosswalk used

---
+++ 01_financial-well-being/0_microdata.R
@@ -0,0 +1,159 @@
+
+###################################################################
+
+# ACS Code: Preparing the microdata, non-subgroup
+# Tina Chelidze 2022-2023
+# Using IPUMS extract for ACS 2021
+# Based on processes developed by Paul Johnson and Kevin Werner in SAS
+# Process:
+# (1) Housekeeping
+# (2) Prepare the Census Place to PUMA crosswalk
+# (3) Prepare for Data Quality flag
+# (4) Prepare Microdata (non-subgroup)
+# (5) Merge the microdata PUMAs to counties
+
+###################################################################
+
+# (1) Housekeeping
+# Set working directory to [gitfolder]: Open mobility-from-poverty.Rproj to make sure all file paths will work
+
+# Libraries you'll need
+library(tidyr)
+library(dplyr)
+library(readr)
+library(tidyverse)
+library(ipumsr)
+
+###################################################################
+
+# (2) Prepare the Census Place to PUMA crosswalk
+# open relevant crosswalk data
+puma_place_2021 <- read_csv("geographic-crosswalks/data/geocorr2012_PUMA_Places_2020.csv")
+
+# rename variables for working purposes
+puma_place_2021 <- puma_place_2021 %>% 
+  dplyr::rename(puma = puma12,
+                statefip = state)
+puma_place_2021 <- puma_place_2021 %>%
+  mutate(statefip = sprintf("%0.2d", as.numeric(statefip)),
+         puma = sprintf("%0.5d", as.numeric(puma)),
+         place = sprintf("%0.5d", as.numeric(place))
+  )
+
+# keep only the variables we will need
+puma_place_2021 <- puma_place_2021 %>% 
+  select(statefip, puma, place, pop20, afact, afact2)
+
+# drop observations where the weight adjustment is zero
+puma_place_2021 <- puma_place_2021[puma_place_2021$afact != 0.000, ]
+# 37810 obs to 37538 obs (272 dropped)
+
+
+# sort by statefip place (previously was statefip puma)
+puma_place_2021 <- puma_place_2021 %>%
+  arrange(statefip, place)
+
+# Create a variable that assigns a weight to each place based on total 2020 population
+# first, create a variable for the total population
+puma_place_2021 <- puma_place_2021 %>%
+  mutate(totpop = sum(pop20))
+
+# then, create a variable for the population of each place (unique statefip+county pairs)
+puma_place_2021 <- puma_place_2021 %>% 
+  dplyr::group_by(statefip, place) %>% 
+  dplyr::mutate(placepop = sum(pop20),
+                placewgt = placepop/totpop)
+
+
+###################################################################
+
+# (3) Prepare for Data Quality flag (at end)
+# Create flags in the PUMA-place crosswalk for high percentage of data from outside of place. 
+# Per Greg (for counties), 75% or more from the county - in this case, place - is good, below 35% is bad, in between is marginal.
+# This is calculated by taking the product of percentage of PUMA in place and
+# percentage of place in PUMA for each place-PUMA pairing, and summing across the place.
+
+# Create new vars of interest
+puma_place <- puma_place_2021 %>%
+  mutate(products = afact*afact2)
+
+puma_place <- puma_place %>%
+  dplyr::group_by(statefip, place) %>%
+  dplyr::mutate(sum_products = sum(products),
+                place_pop = sum(pop20))
+
+summary(puma_place)
+# Q1 of placepop is 351
+# sum_products mean is 0.09
+puma_place <- puma_place %>%
+  dplyr::mutate(puma_flag = case_when((sum_products >= 0.75) ~ 1,
+                                      (sum_products < 0.75 & sum_products >= 0.35) ~ 2,
+                                      (sum_products < 0.35) ~ 3),
+                small_place = case_when((placepop >= 350) ~ 0,
+                                        (placepop < 350) ~ 1))
+
+# save as "puma_place.csv" in gitignore
+write_csv(puma_place, "data/temp/puma_place.csv")
+
+###################################################################
+
+# (4) Prepare Microdata (non-subgroup)
+
+# open IPUMS extract (change "usa_00014.xml" to whatever your extract is called)
+microdata <- 'data/temp/usa_00014.xml'
+ddi <- read_ipums_ddi(microdata)
+acs_2021 <- read_ipums_micro(ddi)
+# 3252599 observations
+
+# keep only vars we need
+acs_2021 <- acs_2021 %>%
+  select(HHWT, ADJUST, STATEFIP, PUMA, GQ, OWNERSHP, OWNCOST, RENT, RENTGRS, HHINCOME,
+         VALUEH, VACANCY, PERNUM, PERWT, EDUC, EDUCD, GRADEATT, EMPSTAT, AGE)
+
+# clean up for matching purposes
+acs_2021 <- acs_2021 %>% 
+  dplyr::rename("puma" = "PUMA",
+                "statefip" = "STATEFIP")
+acs_2021 <- acs_2021 %>%
+  dplyr::mutate(statefip = sprintf("%0.2d", as.numeric(statefip)),
+                puma = sprintf("%0.5d", as.numeric(puma)),
+  )
+
+
+###################################################################
+
+# (5) Merge the microdata PUMAs to places
+
+# (left join, since microdata has more observations)
+memory.limit(size=999999)
+acs2021clean  <- left_join(acs_2021, puma_place, by=c("statefip","puma"))
+# now have 59,650,363 observations
+
+# create a variable for the number of places per PUMA population per place (e.g., in unique statefip+place pairs)
+#acs2021clean  <- acs2021clean  %>%
+#  dplyr::group_by(puma, statefip) %>%
+#  dplyr::mutate(places_per_PUMA = n_distinct(statefip, place))
+
+# create a variable for the number of PUMAs per place 
+#acs2021clean  <- acs2021clean %>%
+#  group_by(place, statefip) %>%
+#  mutate(PUMAs_per_place = n_distinct(statefip, puma))
+
+# Adjust weight to account for PUMA-to-county mapping (those where unique_types > 1).;
+
+# Same adjustments as Kevin:
+acs2021clean <- acs2021clean %>%
+  mutate(HHWT = HHWT*afact,
+         HHINCOME = HHINCOME*ADJUST,
+         PERWT = PERWT*afact,
+         RENTGRS = RENTGRS*ADJUST,
+         OWNCOST = OWNCOST*ADJUST)
+
+# sort by statefip place (previously was statefip puma)
+acs2021clean <- acs2021clean %>%
+  arrange(statefip, place)
+
+# save as "microdata.csv" in gitignore
+write_csv(acs2021clean, "data/temp/2021microdata.csv")
+
+
---
+++ 08_education/preschool.R
@@ -0,0 +1,152 @@
+###################################################################
+
+# ACS Code: Preschool metric, non-subgroup
+# Tina Chelidze 2022-2023
+# Using IPUMS extract for ACS 2021
+# Based on processes developed by Paul Johnson and Kevin Werner in SAS
+# Process:
+
+# Source data: If you need to downloaded from IPUMS, as follows->
+#   
+# - "Select Samples" > ACS (1-year choice; 2021)
+# - "Person" > "Education" > Select "GRADEATT"
+# - "Search" > AGE (& select)
+# - "Search" > PUMA (& select)
+# - "Search" > FIPS (& select state & county fips)
+# - Create Data Extract
+# - in "Options" > "Select Data Quality Flags" > Select "GRADEATT" & "AGE"
+# saved as usa_00004.dat & usa_00004.xml in directory
+
+# (1) Housekeeping
+# (2) Import the prepared microdata file 
+# (3) Calculate the Preschool Metric
+# (4) Create the Data Quality variable
+# (5) Cleaning and export
+
+###################################################################
+
+# (1) Housekeeping
+# Set working directory to [gitfolder]: Open mobility-from-poverty.Rproj to make sure all file paths will work
+
+# Libraries you'll need
+library(tidyr)
+library(dplyr)
+library(readr)
+
+###################################################################
+
+# (2) Import the prepared microdata file 
+
+# Either run "0_microdata.R" OR: Import the already prepared microdata file 
+# this one should already match the PUMAs to places
+# acs2021clean <- read_csv("data/temp/2021microdata.csv")
+
+###################################################################
+
+# (3) Calculate the Preschool Metric
+
+# Isolate the data to only children 3-4 yrs old
+microdata_preschool_age <- acs2021clean %>% 
+  filter(AGE==3|AGE==4) 
+# 1,193,945 obs
+
+# collapse PERWT by place, create a variable for count per place in the collapse
+num_3_and_4 <- microdata_preschool_age %>% 
+  dplyr::group_by(statefip, place) %>% 
+  dplyr::summarize(num_3_and_4 = sum(PERWT),
+            n = n()
+  )
+
+# Isolate further to children 3-4 AND in pre-school 
+microdata_preschool <- acs2021clean %>% 
+  dplyr::filter((AGE==3|AGE==4) & GRADEATT == 1) 
+
+# collapse PERWT by place
+num_in_preschool <- microdata_preschool %>% 
+  dplyr::group_by(statefip, place) %>% 
+  dplyr::summarize(num_in_preschool = sum(PERWT))
+
+
+# Combine the two files to get the ratio we need: share of kids in pre-school
+metrics_preschool <- left_join(num_3_and_4, num_in_preschool, by=c("statefip", "place"))
+
+# Compute the ratio (share of 3-4 yo kids in preschool)
+metrics_preschool <- metrics_preschool %>%
+  mutate(share_in_preschool = num_in_preschool/num_3_and_4)
+
+
+# Create Confidence Interval (CI) and correctly format the variables
+metrics_preschool <- metrics_preschool %>%
+  mutate(not_in_pre = 1 - share_in_preschool,
+         interval = 1.96*sqrt((not_in_pre*share_in_preschool)/n),
+         share_in_preschool_ub = share_in_preschool + interval,
+         share_in_preschool_lb = share_in_preschool - interval)
+
+
+# adjust ub & lb values 
+metrics_preschool$share_in_preschool_ub[metrics_preschool$share_in_preschool_ub > 1] <- 1
+metrics_preschool$share_in_preschool_lb[metrics_preschool$share_in_preschool_lb < 0] <- 0
+
+
+###################################################################
+
+# (4) Create the Data Quality variable
+
+# For Preschool metric: total number of people ages 3 and 4
+metrics_preschool <- metrics_preschool %>% 
+  mutate(size_flag = case_when((num_3_and_4 < 30) ~ 1,
+                               (num_3_and_4 >= 30) ~ 0))
+
+# bring in the PUMA flag file
+# puma_place <- read_csv("C:/Users/tchelidze/Downloads/puma_place.csv")
+
+# Merge the PUMA flag in & create the final data quality metric based on both size and puma flags
+metrics_preschool <- left_join(metrics_preschool, puma_place, by=c("statefip","place"))
+
+# Generate the quality var
+metrics_preschool <- metrics_preschool %>% 
+  mutate(share_in_preschool_quality = case_when(size_flag==0 & puma_flag==1 ~ 1,
+                                             size_flag==0 & puma_flag==2 ~ 2,
+                                             size_flag==0 & puma_flag==3 ~ 3,
+                                             size_flag==1 ~ 3))
+
+###################################################################
+
+# (5) Cleaning and export
+
+# Limit to the Census Places we want 
+
+# rename to prep for merge to places file
+metrics_preschool <- metrics_preschool %>%
+  dplyr::rename(state = statefip)
+
+# first, bring in the places crosswalk (place-populations.csv)
+ places <- read_csv("geographic-crosswalks/data/place-populations.csv")
+# keep only the relevant year (for this, 2020)
+places <- places %>%
+  filter(year > 2019)
+
+# left join to get rid of irrelevant places data
+metrics_preschool <- left_join(places, metrics_preschool, by=c("state","place"))
+metrics_preschool <- metrics_preschool %>% 
+  distinct(year, state, place, share_in_preschool, .keep_all = TRUE)
+
+# add a variable for the year of the data
+metrics_preschool <- metrics_preschool %>%
+  mutate(
+    year = 2021
+  )
+
+# order & sort the variables how we want
+metrics_preschool <- metrics_preschool %>%
+  select(year, state, place, share_in_preschool, 
+         share_in_preschool_ub, share_in_preschool_lb,
+         share_in_preschool_quality)
+
+# Save as "metrics_preschool.csv"
+write_csv(metrics_preschool, "08_education/metrics_preschool_city_2021.csv")  
+
+
+
+
+
