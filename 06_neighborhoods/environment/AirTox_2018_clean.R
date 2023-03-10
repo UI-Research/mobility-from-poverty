bf16d5c142d31aaa744c60bd3e495b07473811f9
rmarx8 <67327033+rmarx8@users.noreply.github.com>
2023-03-10 13:41:33 -0500

    Start of cleaning code

--- 06_neighborhoods/environment/AirTox_2018_clean.R
+++ 06_neighborhoods/environment/AirTox_2018_clean.R
@@ -22,14 +22,12 @@ library(httr)
 library (dplyr)
 library(readxl)
 
-##Import 2018 AirToxScreen Data and 2014 AFFH data
+##Import 2018 AirToxScreen Data and 2014 AFFH data##
 #https://www.epa.gov/AirToxScreen/2018-airtoxscreen-assessment-results#nationwide
 
 cancer_data18 <- read_excel("06_neighborhoods/environment/data/raw/2018_National_CancerRisk_by_tract_srcgrp.xlsx")
 neuro_data18 <- read_excel("06_neighborhoods/environment/data/raw/2018_National_NeurHI_by_tract_srcgrp.xlsx")
 resp_data18 <- read_excel("06_neighborhoods/environment/data/raw/2018_National_RespHI_by_tract_srcgrp.xlsx")
-
-#affh20 <- read.csv("06_neighborhoods/environment/data/raw/Solari_AFFH_tract_AFFHT0006_July2020.csv")
 
 ##Only keep needed variabales##
 
@@ -50,25 +48,11 @@ enviro18_int <- merge(x = resp_cancer18, y = neuro_data18, by = "Tract")
 
 colnames (enviro18_int) <- c("tract", "resp", "carc","neuro")
 
-##drop rows that are not tracts##
-#Rows where the last 6 digist are "0" are not tracts 
-
-#drop first row ("0")
-#enviro18_int = enviro18_int[-1,]
-#turn into charaacter
-#enviro18_int$tract1 <- as.numeric(enviro18_int$tract1)
-#Add leading zero 
-#enviro18_int$tract1 <- str_pad(enviro18_int$tract, 11, side = "left", pad = 0)
-
-#Pull out tract only (last 6 digits)
-#enviro18_int <- enviro18_int %>%
-  #mutate(tract1 = str_sub(tract1, start = 6, end = 11)) 
+##Drop rows that are not tracts##
+#rows where the last 6 digist are "0" are not tracts 
 
 #create a new varibale for filtering (Pull out last 6 digits)
 enviro18_int$tract1 <- str_sub(enviro18_int$tract, start = 6, end = 11)
-
-#format tract1 as number 
-#enviro18_int$tract1 <- as.numeric(enviro18_int$tract1)
 
 #filter to keep only tracts(tract1 != "000000")
 enviro18 <- filter(enviro18_int, tract1 != "000000")
@@ -77,50 +61,7 @@ enviro18 <- filter(enviro18_int, tract1 != "000000")
 enviro18 <- enviro18 %>% 
   select(tract, resp, carc, neuro) 
 
-
-#Keep if 6-digit tract is not "000000" 
-#enviro18_int <- enviro18_int %>%
-  #subset(tract1 != "000000")
-
-#Pull out last 6 digits
-#enviro18_int$tract2 <- enviro18_int %>%
-  #mutate(tract = str_sub(tract, start = -6, end = -1)) 
-
-#enviro18_int$tract.c = as.character(enviro18_int$tract.c)
-#enviro18_int$tract.0 <- sprintf("%.0f", as.numeric(enviro18_int$tract.c))
-#haz_idx18$tract1 <- sprintf("%011d", as.numeric(haz_idx18$tract))
-
-#for(i in nrow(enviro18_int)){
-  #enviro18_int$tract.0[i] = ifelse(nchar(enviro18_int$tract.c[i]==10),paste("0",enviro18_int$tract.c[i],sep = ""),enviro18_int$tract.c[i])  
-#}
-
-#enviro18_int$tract.0[nchar(enviro18_int$tract.c)==10] = c("0",enviro18_int$tract.c[enviro18_int$tract.c]==10)
-
-#pull in tracts 
-#state_fips <- unique(urbnmapr::states$state_fips)
-#tracts18 <- get_acs(geography = "tract",
-                      #variable = "B02001_001",
-                       #state = state_fips,
-                       #geometry = FALSE,
-                       #year = 2018)
-
-#tracts18_2 <- tracts18 %>%
-  #select(GEOID)
-  #colnames(tracts18_2) <- c("tract")
-
-#merge to drop extra rows in AirTox
-#enviro18 <- tidylog::left_join(x = tracts18_2, y = enviro18_int, by = "tract")
-#enviro18$tract <- as.numeric(enviro18$tract)
-
-#check diff between enviro18_int and enviro18
-#not_in_enviro18 <- setdiff(enviro18_int$tract,enviro18$tract)
-#not_in_enviro18 = as.data.frame(not_in_enviro18)
-
-#check diff between enviro18 and tracts18_2
-#tracts18_not_in <- setdiff(tracts18_2$tract,enviro18_int$tract)
-#tracts18_not_in = as.data.frame(tracts18_not_in)
-
-##calculate means and st. devs and store values##
+##Calculate means and st. devs and store values##
 
 resp_mean18 <- mean(enviro18$resp, na.rm = TRUE)
 carc_mean18 <- mean(enviro18$carc, na.rm = TRUE)
@@ -130,69 +71,66 @@ resp_stdv18 <- sd(enviro18$resp, na.rm = TRUE)
 carc_stdv18 <- sd(enviro18$carc, na.rm = TRUE)
 neuro_stdv18 <- sd(enviro18$neuro, na.rm = TRUE)
 
-##Step 6: Calculate EnvHealth components
+##Calculate EnvHealth components##
 enviro18$resp2 <- (enviro18$resp - resp_mean18)/resp_stdv18
 enviro18$carc2 <- (enviro18$carc - carc_mean18)/carc_stdv18
 enviro18$neuro2 <- (enviro18$neuro - neuro_mean18)/neuro_stdv18
 
-##Step 7: Calculate EnvHealth
+##Calculate EnvHealth Indicator##
 enviro18$envhealth18 <- (enviro18$carc2 + enviro18$resp2 + enviro18$neuro2)*-1
 
-##Step 8: Percentile Rank
+#percentile rank caculated values
 enviro18$envhrank18 <- percent_rank(enviro18$envhealth18)
 
-#Step 9: Set haz_idx between 0 and 100 
+#set haz_idx between 0 and 100
 enviro18$haz_idx18 <- round(enviro18$envhrank18*100,0)
 
-#keep what we need
+#keep only the needed variables
 haz_idx18 <- enviro18 %>% 
   select(tract, haz_idx18)
-
-#add leading zeroes 
-#haz_idx18$tract <- str_pad(haz_idx18$tract, 11, side = "left", pad = "0")
-
-
-#keep last six digits?
-#haz_idx18$tract_extra <- haz_idx18 %>%
-  #mutate(tract = str_sub(tract,start = 1, end = 6)) 
 
 
 ##Repeat for 2014##
 
-###Step 1: Import 2014 AirToxScreen Data###
-#https://www.epa.gov/AirToxScreen/2018-airtoxscreen-assessment-results#nationwide
+###Step 1: Import 2014 National Air Toxics Assessment (NATA) Data###
+#https://www.epa.gov/national-air-toxics-assessment/2014-nata-assessment-results
+#hazard index by sourcegroup
 
-cancer_data14 <- read_excel("~/GitHub/mobility-from-poverty/mobility-from-poverty/06_neighborhoods/environment/data/nata2014v2_national_cancerrisk_by_tract_srcgrp.xlsx")
-neuro_data14 <- read_excel("~/GitHub/mobility-from-poverty/mobility-from-poverty/06_neighborhoods/environment/data/nata2014v2_national_neurhi_by_tract_srcgrp.xlsx")
-resp_data14 <- read_excel("~/GitHub/mobility-from-poverty/mobility-from-poverty/06_neighborhoods/environment/data/nata2014v2_national_resphi_by_tract_srcgrp.xlsx")
+cancer_data14 <- read_excel("06_neighborhoods/environment/data/raw/nata2014v2_national_cancerrisk_by_tract_srcgrp.xlsx")
+neuro_data14 <- read_excel("06_neighborhoods/environment/data/raw/nata2014v2_national_neurhi_by_tract_srcgrp.xlsx")
+resp_data14 <- read_excel("06_neighborhoods/environment/data/raw/nata2014v2_national_resphi_by_tract_srcgrp.xlsx")
 
 #Step 2: Only keep needed variabales##
 
 cancer_data14 <- cancer_data14 %>% 
-  select(Tract, Total.Cancer.Risk..per.million.) 
-#convert from factor to numeric
-  cancer_data14$Total.Cancer.Risk..per.million. <- as.numeric(cancer_data14$Total.Cancer.Risk..per.million.)
-  cancer_data14$Total.Cancer.Risk..per.million. <- cancer_data14$Total.Cancer.Risk..per.million. / 1000
+  select(Tract, `Total Cancer Risk (per million)`) 
 
 neuro_data14 <- neuro_data14 %>% 
-  select(Tract, Total.Neurological..hazard.quotient.) 
+  select(Tract, `Total Neurological (hazard quotient)`) 
 
 resp_data14 <- resp_data14 %>% 
-  select(Tract, Total.Respiratory..hazard.quotient.) 
+  select(Tract, `Total Respiratory (hazard quotient)`) 
 
-##Step 3: Join Variables and rename##
-merge(cancer_data14, neuro_data14, by = Tract)
+##Join Variables and rename##
 
 resp_cancer14 <- merge(x = resp_data14, y = cancer_data14, by = "Tract")
 
-enviro14 <- merge(x = resp_cancer14, y = neuro_data14, by = "Tract")
+enviro14_int <- merge(x = resp_cancer14, y = neuro_data14, by = "Tract")
 
-colnames (enviro14) <- c("tract", "resp", "carc","neuro")
+colnames (enviro14_int) <- c("tract", "resp", "carc","neuro")
 
-#drop first row (tract = 0: Total US)
-enviro14 = enviro14[-1,]
 
-##Step 5: Calculate means and st. devs and store values##
+#create a new varibale for filtering (Pull out last 6 digits)
+enviro14_int$tract1 <- str_sub(enviro14_int$tract, start = 6, end = 11)
+
+#filter to keep only tracts(tract1 != "000000")
+enviro14 <- filter(enviro14_int, tract1 != "000000")
+
+#drop tract1
+enviro14 <- enviro14 %>% 
+  select(tract, resp, carc, neuro) 
+
+##Calculate means and st. devs and store values##
 
 resp_mean14 <- mean(enviro14$resp)
 carc_mean14 <- mean(enviro14$carc)
@@ -202,18 +140,18 @@ resp_stdv14 <- sd(enviro14$resp)
 carc_stdv14 <- sd(enviro14$carc)
 neuro_stdv14 <- sd(enviro14$neuro)
 
-##Step 6: Calculate EnvHealth14 components
+##Calculate EnvHealth14 components##
 enviro14$resp2 <- (enviro14$resp - resp_mean14)/resp_stdv14
 enviro14$carc2 <- (enviro14$carc - carc_mean14)/carc_stdv14
 enviro14$neuro2 <- (enviro14$neuro - neuro_mean14)/neuro_stdv14
 
-##Step 7: Calculate EnvHealth14
+##Calculate EnvHealth 2014 Indicator##
 enviro14$envhealth14 <- (enviro14$carc2 + enviro14$resp2 + enviro14$neuro2)*-1
 
-##Step 8: Percentile Rank
+#percentile Rank
 enviro14$envhrank14 <- percent_rank(enviro14$envhealth14)
 
-#Step 9: Set haz_idx between 0 and 100 
+#set haz_idx between 0 and 100 
 enviro14$haz_idx14 <- round(enviro14$envhrank14*100,0)
 
 #keep what we need
@@ -225,63 +163,40 @@ haz_idx14 <- enviro14 %>%
 ##COMPARE TO 2014 AFFH##
 
 #Step 10: Import AFFH data and select haz_idx
-affh20 <- read.csv("06_neighborhoods/environment/data/Solari_AFFH_tract_AFFHT0006_July2020.csv")
+#affh20 <- read.csv("06_neighborhoods/environment/data/raw/AFFH_tract_AFFHT0006_July2020.csv")
 
-affh20 <- affh20 %>% 
-  select(geoid, haz_idx) 
-colnames (affh20) <- c("tract", "haz_idx_affh")
+#affh20 <- affh20 %>% 
+  #select(geoid, haz_idx) 
+  #colnames (affh20) <- c("tract", "haz_idx_affh")
 
 #Step 11: Join indexes by tract to compare values 
 
-haz_idx <- merge(haz_idx14, haz_idx18, by = "tract")
+#haz_idx <- merge(haz_idx14, haz_idx18, by = "tract")
 
-haz_idx <- merge(haz_idx, affh20, by = "tract")
+#haz_idx <- merge(haz_idx, affh20, by = "tract")
 
-haz_idx <- haz_idx[,c(1,4,2,3)]
+#haz_idx <- haz_idx[,c(1,4,2,3)]
 
-haz_idx$diff_14_affh <- haz_idx$haz_idx14 - haz_idx$haz_idx_affh
+#haz_idx$diff_14_affh <- haz_idx$haz_idx14 - haz_idx$haz_idx_affh
 
-haz_idx$chng_14_18 <- haz_idx$haz_idx18 - haz_idx$haz_idx14
+#haz_idx$chng_14_18 <- haz_idx$haz_idx18 - haz_idx$haz_idx14
 
-avg_diff_14_affh <- mean(haz_idx$diff_14_affh)
+#avg_diff_14_affh <- mean(haz_idx$diff_14_affh)
 
-avg_change_14_18 <- mean(haz_idx$chng_14_18)
+#avg_change_14_18 <- mean(haz_idx$chng_14_18)
 
 
 ##Look at which tracts are not in the affh file##
-missing_affh 
+#missing_affh 
 
+#non_affh <- setdiff(enviro14$tract,affh20$tract)
 
-non_affh <- setdiff(enviro14$tract,affh20$tract)
+#non_affh = as.data.frame(non_affh)
 
-non_affh = as.data.frame(non_affh)
-
-
-#write.csv(haz_idx, "EnvHazIdx.csv", row.names = F)
+#write.csv(haz_idx, "06_neighborhoods/environment/data/output/EnvHazIdx2.csv", row.names = F)
 
 
 ##Procude the indicators file##
-
-
-
-#add year variable
-#haz_idx18$year <- 2018 
-
-#add leading zeroes - pad to length of 11 characters
-#haz_idx18$tract <- sprintf("%011d", as.numeric(haz_idx18$tract))
-#invalid format '%011d'; use format %f, %e, %g or %a for numeric objects
-
-#split tract into state and county 
-#haz_idx18 <- haz_idx18 %>%
-  #mutate(state = str_sub(tract, start = 1, end = 2),
-         #county = str_sub(tract, start = 3, end = 5),
-         #tract = str_sub(tract, start = 6, end = 11)) 
-
-#trim tracts to 6-digit id 
-
-#haz_idx18$tract <- sprintf("%011d", as.numeric(haz_idx18$tract))
-
-
 
 ##CROSSWALK## 
 
@@ -300,17 +215,18 @@ crosswalk$tract2 <- paste(crosswalk$state,crosswalk$county,crosswalk$tract)
 crosswalk$tract2 <- gsub(" ", "", crosswalk$tract2)
 crosswalk <- crosswalk %>%
   select(year,tract2)
-colnames(crosswalk) <- c("year", "tract")
+  colnames(crosswalk) <- c("year", "tract")
 
 #look at difference between crosswalk and haz_idx file
 #non_crosswalk <- setdiff(haz_idx18$tract, crosswalk$tract)
 #non_crosswalk = as.data.frame(non_crosswalk)
 
 #merge hazard and crosswalk files 
-hazidx18_merge <- tidylog::left_join(x = crosswalk, y = enviro18_int, 
+hazidx18_merge <- tidylog::left_join(x = crosswalk, y = haz_idx18, 
                         by= "tract")
-#leftjoin #anti_join (see what didn't join) 
 
+#check which did not join 
+#hazidx18_nomerge <- anti_join(crosswalk, haz_idx18, by = "tract")
 
 #split tract into state and county 
 hazidx18_merge <- hazidx18_merge %>%
@@ -321,8 +237,6 @@ hazidx18_merge <- hazidx18_merge %>%
  
 #re-order
 enviro_haz18 <- hazidx18_merge[,c(1,5,6,2,3,4)]
-
-##LOTS OF MISSING, NEED TO FIX##
 
 ##POPULATION WEIGHT TRACTS##
 
@@ -389,8 +303,8 @@ race_pov <- race_pov %>%
 ##Merge with enviro_haz18 data 
 
 #puerto rico is available in the affh data but not apart of our analyses. drop all observations in puerto rico:
-enviro_haz18 <- enviro_haz18 %>%
-  filter(state!= "72")
+#enviro_haz18 <- enviro_haz18 %>%
+  #filter(state!= "72")
 
 #join to race indicator file
 race_enviro <- left_join(race_pov, enviro_haz18, by="GEOID") %>% 
