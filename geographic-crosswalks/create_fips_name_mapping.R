12266cc1090dedc0a5d77fc1186331bd9329e5e9
deckart2 <gdmorrison@uchicago.edu>
2023-03-31 07:58:57 -0400

    use slice_max and bind_rows functions

--- geographic-crosswalks/create_fips_name_mapping.R
+++ geographic-crosswalks/create_fips_name_mapping.R
@@ -7,7 +7,7 @@
 library(tidyverse)
 
 county_xwalk <- read_csv("geographic-crosswalks/data/county-populations.csv") %>%
-  filter(year == max(year)) %>%
+  slice_max(year) %>%
   mutate(fips = paste0(state, county),
         name = paste0(county_name, ", ", state_name)) %>%
   select(fips, 
@@ -15,7 +15,7 @@ county_xwalk <- read_csv("geographic-crosswalks/data/county-populations.csv") %>
   mutate(name2 = name)
 
 city_xwalk <- read_csv("geographic-crosswalks/data/place-populations.csv") %>%
-  filter(year == max(year)) %>%
+  slice_max(year) %>%
   mutate(place_name_2 = sub("\\s+[^ ]+$", "", place_name),
          name = paste0(place_name, ", ", state_name), 
          name2 = paste0(place_name_2, ", ", state_name),
@@ -23,7 +23,7 @@ city_xwalk <- read_csv("geographic-crosswalks/data/place-populations.csv") %>%
   ) %>%
   select(fips, name, name2)
 
-xwalk <- rbind(county_xwalk, city_xwalk)
+xwalk <- bind_rows(county_xwalk, city_xwalk)
 write_csv(xwalk, "geographic-crosswalks/data/names-to-fips_all.csv")  
 write_csv(county_xwalk, "geographic-crosswalks/data/names-to-fips_county.csv")
 write_csv(city_xwalk, "geographic-crosswalks/data/names-to-fips_city.csv")
