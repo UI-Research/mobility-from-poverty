2601d3a02f78b8081b5087cf6c67a8aec99bf0e6
jwalsh28 <jwalsh@urban.org>
2024-03-04 09:57:58 -0500

    Latest version of income metric

--- functions/API/ipums_repwt_household.R
+++ functions/API/ipums_repwt_household.R
@@ -42,7 +42,7 @@ ipums_repwt_household <- function(extract_name, extract_description, survey){
            #        case_selections = c("1")),
           var_spec("GQ",
                    case_selections = c("1", "2", "5")),
-          "REPWTP",
+          "REPWT",
           "CBPERNUM"
         )
       )
@@ -88,7 +88,7 @@ ipums_repwt_household <- function(extract_name, extract_description, survey){
   acs_imported <- micro_data %>%
     rename_with(tolower) %>% 
     select(-serial, -strata, -cluster, -year,
-           -pernum, -perwt, -hhwt, -gq, -age) %>% 
+           -pernum, -perwt, -hhwt, -gq) %>% 
     mutate(sample = as_factor(sample),
            unique_person_id = paste0(sample, cbserial, cbpernum))
 
