0b9efbe79b202d08457aaf00db71ea0bb68c7483
jwalsh28 <jwalsh@urban.org>
2024-03-05 12:19:42 -0500

    Update to the preschool program. Calculate only with 5-year data. Use custom functions for survey_mean calculation. For now keep Kindergarten attending 3-4 year olds out of numerator and denominator

---
+++ 08_education/R/calc_survey_mean.R
@@ -0,0 +1,19 @@
+calc_survey_mean <- function(.data, .geo_level) {
+  
+  .data %>%
+    as_survey_rep(
+      weights = perwt, 
+      repweights = matches("repwtp[0-9]+"),
+      type = "JK1",
+      scale = 4 / 80,
+      rscales = rep(1, 80), 
+      mse = TRUE
+    ) %>% 
+    group_by(year, crosswalk_period, statefip, {{ .geo_level }}) %>% 
+    summarise(
+      share_in_preschool = survey_mean(preschool, vartype = "ci"),
+      n = n()
+    ) %>%
+    ungroup()
+  
+}
