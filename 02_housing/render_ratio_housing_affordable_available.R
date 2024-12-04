3013c345a1761713afd651768fba7e052b1a06c6
wcurrangroome <wcurrangroome@gmail.com>
2024-12-03 22:34:13 -0500

    minor updates to code and rendering function

--- 02_housing/render_ratio_housing_affordable_available.R
+++ 02_housing/render_ratio_housing_affordable_available.R
@@ -4,29 +4,31 @@
 library(tidyverse)
 library(quarto)
 library(here)
-
-setwd(here("02_housing"))
+library(xfun)
 
 parameters = tibble(
   geography = c("county", "place"),
-  county_years = list(c(2021, 2022), c(2021, 2022)),
-  place_years = list(c(2021, 2022), c(2021, 2022))) %>%
+  county_years = list(c(2022))) %>%
   mutate(
+    place_years = county_years,
     years = map_chr(county_years, ~ str_c(.x, collapse = "_")),
     output_file = str_c("ratio_housing_affordable_available_", geography, "_", years, ".html"),
     execute_params = pmap(
-      list(geography, county_years, place_years),
-      function(geography, county_years, place_years) { 
+      list(geography, county_years, place_years, output_file),
+      function(geography, county_years, place_years, output_file) { 
         list(
           geography = geography, 
+          output_file = output_file,
           county_years = county_years,
           place_years = place_years)})) %>% 
-  select(-c(geography, county_years, place_years, years))
+  select(-c(geography, county_years, place_years, years, output_file))
 
-pwalk(
-  .l = parameters,                     
-  .f = quarto_render, 
-  input = "ratio_housing_affordable_available.qmd",
-  output_format = "all",
-  execute = TRUE,
-  .progress = TRUE)
+in_dir(
+  dir = here("02_housing"),
+  expr = pwalk(
+    .l = parameters,                     
+    .f = quarto_render, 
+    input = "ratio_housing_affordable_available.qmd",
+    output_format = "all",
+    execute = TRUE,
+    .progress = TRUE))
