d092b1a4ab6a82d7e7f34d6de0006bc4f65a7ffe
Manuel Alcalá Kovalski <malcalakovalski@urban.org>
2026-02-09 14:42:16 -0500

    Render county and place files

--- 12_housing-stability/run_homelessness.R
+++ 12_housing-stability/run_homelessness.R
@@ -7,11 +7,13 @@ library(here)
 # Render for county
 quarto_render(
   here("12_housing-stability", "homelessness.qmd"),
+  output_file = "homelessness-county.html",
   execute_params = list(geography = "county", years = 2019:2022)
 )
 
 # Render for place
 quarto_render(
   here("12_housing-stability", "homelessness.qmd"),
+  output_file = "homelessness-place.html",
   execute_params = list(geography = "place", years = 2019:2022)
 )
