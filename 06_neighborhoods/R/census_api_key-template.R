7ae4830dbf040e68664578d1d19f2c2dc74b0e29
awunderground <aaronrwilliams100@gmail.com>
2026-05-13 10:19:30 -0400

    Unpack the 08_education/

--- 06_neighborhoods/R/census_api_key-template.R
+++
@@ -1,6 +0,0 @@
-# Add key to .Renviron
-Sys.setenv(CENSUS_KEY = "")
-# Reload .Renviron
-readRenviron("~/.Renviron")
-# Check to see that the expected key is output in your R console
-Sys.getenv("CENSUS_KEY")
