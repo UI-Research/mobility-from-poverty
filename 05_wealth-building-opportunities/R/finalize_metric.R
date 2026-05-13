564724340ef8e6aa096d5aa1fad57cde438fe7f8
awunderground <aaronrwilliams100@gmail.com>
2026-05-12 20:08:07 -0400

    Add a few leftover files from the previous migrations.

---
+++ 05_wealth-building-opportunities/R/finalize_metric.R
@@ -0,0 +1,18 @@
+finalize_metric <- function(.data, .geo_level) {
+  
+  .data %>%
+    arrange(year, statefip, {{ .geo_level }}) %>%
+    rename(
+      pctl_income_20_lb = pctl_income_q20_low,
+      pctl_income_20_ub = pctl_income_q20_upp,
+      pctl_income_20 = pctl_income_q20,
+      pctl_income_50_lb = pctl_income_q50_low,
+      pctl_income_50_ub = pctl_income_q50_upp,
+      pctl_income_50 = pctl_income_q50,
+      pctl_income_80_lb = pctl_income_q80_low,
+      pctl_income_80_ub = pctl_income_q80_upp,
+      pctl_income_80 = pctl_income_q80,
+    ) %>% 
+    select(-weighted_n)
+  
+}
